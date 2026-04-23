fc0445461fcfa30749d52a8250b0e897d3ba9432
Bruno Oliveira <brunobrigida2327@gmail.com>
2026-04-23 17:24:33 -0300

    espaçamento da umidade,vento e sensaçao

--- src/components/temperatura.jsx
+++ src/components/temperatura.jsx
@@ -1,12 +1,17 @@
 function Temperatura({dados}){
     return(
         <div className="weather-card">
+            
             <h2>{dados.cidade}</h2>
             <p className="temperatura">{dados.temperatura}°C</p>
-            <p> Sensação: {dados.sensacao}°C</p>
-            <p> Umidade: {dados.umidade}%</p>
-            <p> Vento: {dados.vento} km/h</p>
+            
+            <div className="info-extra">
+                 <p>🌡️ Sensação: {dados.sensacao}°C</p>
+                 <p>💧 Umidade: {dados.umidade}%</p>
+                <p>💨 Vento: {dados.vento} km/h</p>
+            </div>
         </div>
+
     )
 }
 export default Temperatura
