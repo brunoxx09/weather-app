882079624574ca40240af590daec78b70dfad318
Bruno Oliveira <brunobrigida2327@gmail.com>
2026-04-23 17:18:29 -0300

    adiciona sensacao umidade e vento

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,53 @@
  import Temperatura from "./components/temperatura"
  import MensagemDeErro from "./components/mensagemdeerro"
  import './App.css'
- function App(){
+  function App(){
   
-  const [clima,setclima]= useState(null)
-  const [erro,seterro]= useState(null)
-  const [carregado,setcarregado]=useState(false)
+    const [clima,setclima]= useState(null)
+    const [erro,seterro]= useState(null)
+    const [carregado,setcarregado]=useState(false)
   
-  async function buscarclima(cidade){
-   setclima(null)
-   seterro(null)
-   setcarregado(true)
+    async function buscarclima(cidade){
+      setclima(null)
+      seterro(null)
+      setcarregado(true)
   
- const resposta = await fetch(
-  `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`
-)
-const dados = await resposta.json()
+    const resposta = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`
+    )
+    const dados = await resposta.json()
 
-if (!dados.results) {   
-    seterro('Cidade não encontrada')
+      if (!dados.results) {   
+      seterro('Cidade não encontrada')
+      setcarregado(false)
+      return
+    }
+
+    const {latitude,longitude,name:nome}= dados.results[0]
+
+  //api para humidade,sensaçao e vento
+    const respostaclima = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,apparent_temperature,relative_humidity_2m,wind_speed_10m&timezone=auto`
+    )
+    const dadosclima = await respostaclima.json()
+
+    setclima({
+      cidade: nome,
+      temperatura: dadosclima.current.temperature_2m,
+      sensacao: dadosclima.current.apparent_temperature,
+      umidade: dadosclima.current.relative_humidity_2m,
+      vento: dadosclima.current.wind_speed_10m
+    })
     setcarregado(false)
-    return
   }
 
-  const {latitude,longitude,name:nome}= dados.results[0]
-
-const respostaclima = await fetch(
-  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode&timezone=auto`
-)
-const dadosclima = await respostaclima.json()
-
-setclima({
-  cidade:nome,
-  temperatura:dadosclima.current.temperature_2m
-})
-setcarregado(false)
-
-}
-
-return (
-    <div className="container">
-      <h1>Clima das cidades</h1>
-      <BarraDeBusca onBuscar={buscarclima}/>
-      {clima && <Temperatura dados={clima}/>}
-      {erro && <MensagemDeErro mensagem={erro}/>}
-    </div>
-   )
-}
-export default App
+    return (
+      <div className="container">
+        <h1>Clima das cidades</h1>
+        <BarraDeBusca onBuscar={buscarclima}/>
+        {clima && <Temperatura dados={clima}/>}
+        {erro && <MensagemDeErro mensagem={erro}/>}
+      </div>
+    )
+  }
+  export default App
--- src/components/temperatura.jsx
+++ src/components/temperatura.jsx
@@ -3,6 +3,9 @@ function Temperatura({dados}){
         <div className="weather-card">
             <h2>{dados.cidade}</h2>
             <p className="temperatura">{dados.temperatura}°C</p>
+            <p> Sensação: {dados.sensacao}°C</p>
+            <p> Umidade: {dados.umidade}%</p>
+            <p> Vento: {dados.vento} km/h</p>
         </div>
     )
 }
